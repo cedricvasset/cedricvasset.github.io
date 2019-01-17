e41d977881321decbe3a9259d97fa18bc71b9453
cedricvasset <vasset.cedric@orange.fr>
2019-01-17 15:00:51 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.defilright').click(function(){
+    $('#music').show();
+    $('#gaming').hide();
+  });
+  $('.defilleft').click(function(){
+    $('#music').hide();
+    $('#gaming').show();
+  });
+});
